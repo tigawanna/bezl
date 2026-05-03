268166b9265bcb1ccde3697626731bf60b83b639
David Amunga <david.onyimbo@gmail.com>
2026-05-03 21:10:26 +0300

    feat: added workflows

---
+++ Formula/bezl.rb
@@ -0,0 +1,19 @@
+class Bezl < Formula
+  desc "Add device frames to device screen recordings"
+  homepage "https://github.com/davidamunga/bezl"
+  url "https://registry.npmjs.org/bezl/-/bezl-VERSION.tgz"
+  sha256 "SHA256_PLACEHOLDER"
+  license "MIT"
+  version "VERSION"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bezl --version 2>&1")
+  end
+end
